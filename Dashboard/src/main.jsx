fc6e80a0f287ee1675ac369b000f46fafbeaaaa4
Diganta <digantachakraborty688@gmail.com>
2025-02-10 15:55:23 +0530

    changing route

--- Dashboard/src/main.jsx
+++ Dashboard/src/main.jsx
@@ -1,18 +1,12 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import Home from "./Components/Home";
 import NotFound from "./Components/NotFound";
 import "./index.css";
 
-const RedirectToDashboard = () => {
-  const defaultId = "123"; 
-  return <Navigate to={`/dashboard/${defaultId}`} replace />;
-};
-
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<RedirectToDashboard />} />
       <Route path="/dashboard/:id/*" element={<Home />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
